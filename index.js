6103786f5abfdaf4ed154e745d9881f362d960a9
TomWhitwell <tom.whitwell@gmail.com>
2016-04-20 14:50:22 +0100

    fixing

--- index.js
+++ index.js
@@ -123,7 +123,16 @@ function sendAnimalMessage(sender) {
 
                         "payload": "Person",
                     }],
-                }, ]
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://lorempixel.com/400/200/cats/",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
             }
         }
     }
